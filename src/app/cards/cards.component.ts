50933819a0ab36694d014cddcb92a428f212ac28
tutkuekinsuakgun <tutkuekinsuakgun@etu.edu.tr>
2024-12-26 22:25:53 +0300

    json changed

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -11,7 +11,7 @@ export class CardsComponent {
   cards = [
     { provider: 'Ixaris', reference: '348h4_34324', cardNumber: '1234 5678 9012 9839', balance: '45₺', imageUrl: 'assets/shiba.jpg' },
     { provider: 'Sunrate', reference: '34223gfg45345', cardNumber: '3567 6846 8937 7846', balance: '433₺', imageUrl: 'assets/shiba.jpg' },
-    { provider: 'USBannk', reference: '4657e564342', cardNumber: '7583 5840 4843 4840', balance: '4324₺', imageUrl: 'assets/shiba.jpg' },
+    { provider: 'USBank', reference: '4657e564342', cardNumber: '7583 5840 4843 4840', balance: '4324₺', imageUrl: 'assets/shiba.jpg' },
     { provider: 'Connexpay', reference: 'sdsasedf455', cardNumber: '5748 7382 3738 3839', balance: '0₺', imageUrl: 'assets/shiba.jpg' },
     { provider: 'Voxel', reference: 'fefefe6546656', cardNumber: '4682 3848 3829 9284', balance: '3423₺', imageUrl: 'assets/shiba.jpg' },
     { provider: 'Amadeus', reference: '34343255_4fsd', cardNumber: '5738 3234 3748 8329', balance: '2₺', imageUrl: 'assets/shiba.jpg' },
